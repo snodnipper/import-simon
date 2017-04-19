4557aa980b0917f775ba20c9cba6fb3ef847a97b
Oliver Snowden <oliversnowden@snodnipper.co.uk>
2017-04-20 00:10:40 +0100

    tidy top level directory

--- projects/os-unplugged/env.sh
+++ projects/os-unplugged/env.sh
@@ -6,6 +6,8 @@ if [[ $_ == $0 ]]; then
   echo "  . env.sh"
   echo ""
 fi
+export WWW_DATA="https://yourserver.com/www.tar.gz"
+export Z2_DOWNLOAD_LOCATION="https://yourserver.com/data_Z2.mbtiles"
 export Z12_DOWNLOAD_LOCATION="https://yourserver.com/data_Z12.mbtiles"
 export Z15_DOWNLOAD_LOCATION="https://yourserver.com/data_Z15.mbtiles"
 
